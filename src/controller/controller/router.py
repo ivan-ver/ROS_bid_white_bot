a7510a3ea2c5dee0491d6e007577b57a83182996
ivan-ver <ivan_ver89@mail.ru>
2023-01-30 22:54:26 +0300

    Add inverse kinematic

--- src/controller/controller/router.py
+++ src/controller/controller/router.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, MultiArrayLayout
-from bid_wb_interfaces.msg import TargetInfo
 from geometry_msgs.msg import Accel
+from bid_wb_interfaces.msg import CurrentState
 
 
 class Router(Node):
@@ -11,10 +10,22 @@ class Router(Node):
         super().__init__("router")
         self.interface_subscriber = self.create_subscription(Accel, 'move_to_coordinates', self.listener_callback, 10)
         self.interface_publisher = self.create_publisher(Accel, 'publish_state', 10)
+        self.current_state_publisher = self.create_publisher(CurrentState, 'current_state', 10)
+        self.create_timer(0.5, self.publish_current_state)
 
     def listener_callback(self, msg: Accel) -> None:
         self.get_logger().info('I heard: "%s"' % msg)
         self.interface_publisher.publish(msg)
+
+    def publish_current_state(self):
+        current_state = CurrentState()
+        current_state.coordinates.x = 800.0
+        current_state.coordinates.y = 500.0
+        current_state.coordinates.z = 500.0
+        current_state.alpha_angle = 45.0
+        current_state.betta_angle = 45.0
+        current_state.current_engine_tics = [654, 258, 317, 943, 234, 745]
+        self.current_state_publisher.publish(current_state)
 
 
 def main(args=None) -> None:
